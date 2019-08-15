62a87f716b1f87075c911b9c1363f2addc850cba
Nick Rice <nicholas.rice119@gmail.com>
2019-08-15 12:20:23 -0700

    adding pre/post plugins

--- packages/fast-components-styles-msft/perf.js
+++ packages/fast-components-styles-msft/perf.js
@@ -95,13 +95,31 @@ import {
     neutralLayerL2,
     neutralLayerL3,
     neutralLayerL4,
+    neutralFocus,
+    neutralFocusInnerAccent
 } from "./src/utilities/color";
 const Benchmark = require("benchmark");
 import fs from "fs";
 
+// Stylesheet dependencies
+import { applyScaledTypeRamp } from "./src/utilities/typography";
+import { applyCursorPointer } from "./src/utilities/cursor";
+import { applyCornerRadius, applyFocusPlaceholderBorder } from "./src/utilities/border";
+import { focusOutlineWidth, outlineWidth } from "./src/utilities/design-system";
+import { applyDisabledState } from "./src/utilities/disabled";
+import { glyphSize, height, horizontalSpacing } from "./src/utilities/density";
+import { getDesignSystemValue } from "./src/design-system"
+import {
+    applyFocusVisible,
+    directionSwitch,
+    format,
+    toPx,
+} from "@microsoft/fast-jss-utilities";
+
 var suite = new Benchmark.Suite();
 const jssWithPlugins = create(preset());
 const jssWithoutPlugins = create();
+jssWithoutPlugins.use(jssNested())
 
 const colorRecipes = [
     [neutralForegroundRest, "neutralForegroundRest"],
@@ -198,6 +216,7 @@ const stylesheetRenderers = [
     [() => {jssWithPlugins.createStyleSheet(TypographyStyles).update(DesignSystem)}, "TypographyStyles"],
 ]
 
+const density = getDesignSystemValue("density");
 const pluginTestSimpleStyleSheet = {
     className: {
         color: "red",
@@ -211,9 +230,472 @@ const pluginTestSimpleStyleSheet = {
     }
 }
 
+const buttonStylesWithAllPlugins = {
+    button: {
+        ...applyScaledTypeRamp("t7"),
+        fontFamily: "inherit",
+        ...applyCursorPointer(),
+        boxSizing: "border-box",
+        maxWidth: "374px",
+        minWidth: (designSystem) => density(designSystem) <= -2 ? "28px" : "32px",
+        padding: format("0 {0}", horizontalSpacing(focusOutlineWidth)),
+        display: "inline-flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: height(),
+        ...applyFocusPlaceholderBorder(),
+        ...applyCornerRadius(),
+        lineHeight: "1",
+        overflow: "hidden",
+        textDecoration: "none",
+        whiteSpace: "nowrap",
+        transition: "all 0.1s ease-in-out",
+        color: neutralForegroundRest,
+        fill: neutralForegroundRest,
+        background: neutralFillRest,
+        "&:hover:enabled": {
+            background: neutralFillHover,
+        },
+        "&:active:enabled": {
+            background: neutralFillActive,
+        },
+        ...applyFocusVisible({
+            borderColor: neutralFocus,
+        }),
+        "&::-moz-focus-inner": {
+            border: "0",
+        },
+        "@media (-ms-high-contrast:active)": {
+            fill: "ButtonHighlight",
+        },
+    },
+    button__primary: {
+        color: accentForegroundCut,
+        fill: accentForegroundCut,
+        background: accentFillRest,
+        "&:hover:enabled": {
+            background: accentFillHover,
+        },
+        "&:active:enabled": {
+            background: accentFillActive,
+        },
+        ...applyFocusVisible({
+            borderColor: neutralFocus,
+            boxShadow: format(
+                "0 0 0 {0} inset {1}",
+                toPx(focusOutlineWidth),
+                neutralFocusInnerAccent(accentFillRest)
+            ),
+        }),
+        "& $button_beforeContent, & $button_afterContent": {
+            fill: accentForegroundCut,
+        },
+    },
+    button__outline: {
+        background: "transparent",
+        border: format(
+            "{0} solid {1}",
+            toPx(outlineWidth),
+            neutralOutlineRest
+        ),
+        padding: format("0 {0}", horizontalSpacing(outlineWidth)),
+        "&:hover:enabled": {
+            background: "transparent",
+            border: format(
+                "{0} solid {1}",
+                toPx(outlineWidth),
+                neutralOutlineHover
+            ),
+        },
+        "&:active:enabled": {
+            background: "transparent",
+            border: format(
+                "{0} solid {1}",
+                toPx(outlineWidth),
+                neutralOutlineActive
+            ),
+        },
+        ...applyFocusVisible({
+            boxShadow: 
+            (designSystem) => {
+                return `0 0 0 ${toPx(
+                    focusOutlineWidth(designSystem) - outlineWidth(designSystem)
+                )} ${neutralFocus(designSystem)} inset`;
+            }
+            ,
+            borderColor: neutralFocus,
+        }),
+    },
+    button__lightweight: {
+        color: accentForegroundRest,
+        fill: accentForegroundRest,
+        background: "transparent",
+        ...applyFocusVisible({
+            borderColor: "transparent",
+            boxShadow: "none",
+            "& $button_contentRegion::before": {
+                background: neutralForegroundRest,
+                height: toPx(focusOutlineWidth),
+            },
+        }),
+        // Underline
+        "&:hover $button_contentRegion::before": {
+            background: accentForegroundHover,
+            "@media (-ms-high-contrast:active)": {
+                background: "ButtonHighlight",
+            },
+        },
+        "&:hover$button__disabled $button_contentRegion::before": {
+            display: "none",
+        },
+        "&:active $button_contentRegion::before": {
+            background: accentForegroundActive,
+        },
+        "&$button__disabled, &$button__disabled $button_contentRegion::before": {
+            background: "transparent"
+        },
+        "@media (-ms-high-contrast:active)": {
+            border: "none",
+            fill: "ButtonHighlight",
+        },
+        "&:hover:enabled": {
+            color: accentForegroundHover,
+            fill: accentForegroundHover,
+            background: "transparent"
+        },
+        "&:active:enabled": {
+            color: accentForegroundActive,
+            fill: accentForegroundActive,
+            background: "transparent"
+        },
+    },
+    button__justified: {
+        color: accentForegroundRest,
+        fill: accentForegroundRest,
+        background: "transparent",
+        ...applyFocusVisible({
+            borderColor: "transparent",
+            boxShadow: "none",
+            "& $button_contentRegion::before": {
+                background: neutralForegroundRest,
+                height: toPx(focusOutlineWidth),
+            },
+        }),
+        // Underline
+        "&:hover $button_contentRegion::before": {
+            background: accentForegroundHover,
+            "@media (-ms-high-contrast:active)": {
+                background: "ButtonHighlight",
+            },
+        },
+        "&:hover$button__disabled $button_contentRegion::before": {
+            display: "none",
+        },
+        "&:active $button_contentRegion::before": {
+            background: accentForegroundActive,
+        },
+        "&$button__disabled, &$button__disabled $button_contentRegion::before": {
+            background: "transparent"
+        },
+        "@media (-ms-high-contrast:active)": {
+            border: "none",
+            fill: "ButtonHighlight",
+        },
+        "&:hover:enabled": {
+            color: accentForegroundHover,
+            fill: accentForegroundHover,
+            background: "transparent"
+        },
+        "&:active:enabled": {
+            color: accentForegroundActive,
+            fill: accentForegroundActive,
+            background: "transparent"
+        },
+        minWidth: "74px",
+        paddingLeft: "0",
+        paddingRight: "0",
+        borderWidth: "0",
+        justifyContent: "flex-start",
+    },
+    button__stealth: {
+        background: neutralFillStealthRest,
+        "&:hover:enabled": {
+            backgroundColor: neutralFillStealthHover,
+        },
+        "&:active:enabled": {
+            backgroundColor: neutralFillStealthActive,
+        },
+        ...applyFocusVisible({
+            borderColor: neutralFocus,
+        }),
+    },
+    button_contentRegion: {
+        position: "relative",
+        "&::before": {
+            content: "''",
+            display: "block",
+            height: toPx(outlineWidth),
+            position: "absolute",
+            bottom: "-3px",
+            width: "100%",
+            left: directionSwitch("0", ""),
+            right: directionSwitch("", "0"),
+        },
+        "& svg": {
+            width: glyphSize,
+            height: glyphSize,
+        },
+    },
+    button__disabled: {
+        ...applyDisabledState(),
+        "& $button_beforeContent, & $button_afterContent": {
+            fill: "ButtonHighlight",
+        },
+    },
+    button_beforeContent: {
+        width: glyphSize,
+        height: glyphSize,
+    },
+    button_afterContent: {
+        width: glyphSize,
+        height: glyphSize,
+    },
+};
+const buttonStylesWithoutAllPlugins = {
+    button: {
+        ...applyScaledTypeRamp("t7"),
+        "font-family": "inherit",
+        ...applyCursorPointer(),
+        "box-sizing": "border-box",
+        "max-width": "374px",
+        "min-width": (designSystem) => density(designSystem) <= -2 ? "28px" : "32px",
+        padding: format("0 {0}", horizontalSpacing(focusOutlineWidth)),
+        display: "inline-flex",
+        "justify-content": "center",
+        "align-items": "center",
+        height: height(),
+        ...applyFocusPlaceholderBorder(),
+        ...applyCornerRadius(),
+        "line-height": "1",
+        overflow: "hidden",
+        "text-decoration": "none",
+        "white-space": "nowrap",
+        transition: "all 0.1s ease-in-out",
+        color: neutralForegroundRest,
+        fill: neutralForegroundRest,
+        background: neutralFillRest,
+        "&:hover:enabled": {
+            background: neutralFillHover,
+        },
+        "&:active:enabled": {
+            background: neutralFillActive,
+        },
+        ...applyFocusVisible({
+            "border-color": neutralFocus,
+        }),
+        "&::-moz-focus-inner": {
+            border: "0",
+        },
+        "@media (-ms-high-contrast:active)": {
+            fill: "ButtonHighlight",
+        },
+    },
+    button__primary: {
+        color: accentForegroundCut,
+        fill: accentForegroundCut,
+        background: accentFillRest,
+        "&:hover:enabled": {
+            background: accentFillHover,
+        },
+        "&:active:enabled": {
+            background: accentFillActive,
+        },
+        ...applyFocusVisible({
+            "border-color": neutralFocus,
+            boxShadow: format(
+                "0 0 0 {0} inset {1}",
+                toPx(focusOutlineWidth),
+                neutralFocusInnerAccent(accentFillRest)
+            ),
+        }),
+        "& $button_beforeContent, & $button_afterContent": {
+            fill: accentForegroundCut,
+        },
+    },
+    button__outline: {
+        background: "transparent",
+        border: format(
+            "{0} solid {1}",
+            toPx(outlineWidth),
+            neutralOutlineRest
+        ),
+        padding: format("0 {0}", horizontalSpacing(outlineWidth)),
+        "&:hover:enabled": {
+            background: "transparent",
+            border: format(
+                "{0} solid {1}",
+                toPx(outlineWidth),
+                neutralOutlineHover
+            ),
+        },
+        "&:active:enabled": {
+            background: "transparent",
+            border: format(
+                "{0} solid {1}",
+                toPx(outlineWidth),
+                neutralOutlineActive
+            ),
+        },
+        ...applyFocusVisible({
+            boxShadow: 
+            (designSystem) => {
+                return `0 0 0 ${toPx(
+                    focusOutlineWidth(designSystem) - outlineWidth(designSystem)
+                )} ${neutralFocus(designSystem)} inset`;
+            }
+            ,
+            "border-color": neutralFocus,
+        }),
+    },
+    button__lightweight: {
+        color: accentForegroundRest,
+        fill: accentForegroundRest,
+        background: "transparent",
+        ...applyFocusVisible({
+            "border-color": "transparent",
+            "box-shadow": "none",
+            "& $button_contentRegion::before": {
+                background: neutralForegroundRest,
+                height: toPx(focusOutlineWidth),
+            },
+        }),
+        // Underline
+        "&:hover $button_contentRegion::before": {
+            background: accentForegroundHover,
+            "@media (-ms-high-contrast:active)": {
+                background: "ButtonHighlight",
+            },
+        },
+        "&:hover$button__disabled $button_contentRegion::before": {
+            display: "none",
+        },
+        "&:active $button_contentRegion::before": {
+            background: accentForegroundActive,
+        },
+        "&$button__disabled, &$button__disabled $button_contentRegion::before": {
+            background: "transparent"
+        },
+        "@media (-ms-high-contrast:active)": {
+            border: "none",
+            fill: "ButtonHighlight",
+        },
+        "&:hover:enabled": {
+            color: accentForegroundHover,
+            fill: accentForegroundHover,
+            background: "transparent"
+        },
+        "&:active:enabled": {
+            color: accentForegroundActive,
+            fill: accentForegroundActive,
+            background: "transparent"
+        },
+    },
+    button__justified: {
+        color: accentForegroundRest,
+        fill: accentForegroundRest,
+        background: "transparent",
+        ...applyFocusVisible({
+            "border-color": "transparent",
+            "box-shadow": "none",
+            "& $button_contentRegion::before": {
+                background: neutralForegroundRest,
+                height: toPx(focusOutlineWidth),
+            },
+        }),
+        // Underline
+        "&:hover $button_contentRegion::before": {
+            background: accentForegroundHover,
+            "@media (-ms-high-contrast:active)": {
+                background: "ButtonHighlight",
+            },
+        },
+        "&:hover$button__disabled $button_contentRegion::before": {
+            display: "none",
+        },
+        "&:active $button_contentRegion::before": {
+            background: accentForegroundActive,
+        },
+        "&$button__disabled, &$button__disabled $button_contentRegion::before": {
+            background: "transparent"
+        },
+        "@media (-ms-high-contrast:active)": {
+            border: "none",
+            fill: "ButtonHighlight",
+        },
+        "&:hover:enabled": {
+            color: accentForegroundHover,
+            fill: accentForegroundHover,
+            background: "transparent"
+        },
+        "&:active:enabled": {
+            color: accentForegroundActive,
+            fill: accentForegroundActive,
+            background: "transparent"
+        },
+        minWidth: "74px",
+        paddingLeft: "0",
+        paddingRight: "0",
+        borderWidth: "0",
+        "justify-content": "flex-start",
+    },
+    button__stealth: {
+        background: neutralFillStealthRest,
+        "&:hover:enabled": {
+            "background-color": neutralFillStealthHover,
+        },
+        "&:active:enabled": {
+            "background-color": neutralFillStealthActive,
+        },
+        ...applyFocusVisible({
+            "border-color": neutralFocus,
+        }),
+    },
+    button_contentRegion: {
+        position: "relative",
+        "&::before": {
+            content: "''",
+            display: "block",
+            height: toPx(outlineWidth),
+            position: "absolute",
+            bottom: "-3px",
+            width: "100%",
+            left: directionSwitch("0", ""),
+            right: directionSwitch("", "0"),
+        },
+        "& svg": {
+            width: glyphSize,
+            height: glyphSize,
+        },
+    },
+    button__disabled: {
+        ...applyDisabledState(),
+        "& $button_beforeContent, & $button_afterContent": {
+            fill: "ButtonHighlight",
+        },
+    },
+    button_beforeContent: {
+        width: glyphSize,
+        height: glyphSize,
+    },
+    button_afterContent: {
+        width: glyphSize,
+        height: glyphSize,
+    },
+};
+
 const jssPluginTests = [
-    [() => {jssWithPlugins.createStyleSheet(pluginTestSimpleStyleSheet).update(DesignSystem)}, "withPlugins"],
-    [() => {jssWithoutPlugins.createStyleSheet(pluginTestSimpleStyleSheet).update(DesignSystem)}, "withoutPlugins"],
+    [() => {jssWithPlugins.createStyleSheet(buttonStylesWithAllPlugins).update(DesignSystem)}, "withPlugins"],
+    [() => {jssWithoutPlugins.createStyleSheet(buttonStylesWithoutAllPlugins).update(DesignSystem)}, "withoutPlugins"],
 ]
 
 function createSuite(data) {
@@ -225,7 +707,7 @@ function createSuite(data) {
 }
 
 
-createSuite(jssPluginTests)
+createSuite(stylesheetRenderers)
 
 suite.on("complete", function() {
     const body = Object.keys(this)
@@ -235,7 +717,12 @@ suite.on("complete", function() {
             ops: this[key].hz,
             opsMs: this[key].hz / 1000,
         }));
-    fs.writeFileSync("./jss-plugins.json", JSON.stringify(body, null, 4));
+    fs.writeFileSync("./jss-plugins.pre-plugin-removal.json", JSON.stringify(body, null, 4));
 });
+
+suite.on("error", (arg) => {
+    console.log(JSON.stringify(arg, null, 2))
+    throw new Error(arg.message)
+})
 
 suite.run();
