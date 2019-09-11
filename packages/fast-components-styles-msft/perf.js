6f7003b99e5ecbb8c51db5ab311235c9d42df6bf
Nick Rice <nicholas.rice119@gmail.com>
2019-09-11 13:29:17 -0700

    post changes

--- packages/fast-components-styles-msft/perf.js
+++ packages/fast-components-styles-msft/perf.js
@@ -3,7 +3,7 @@ import DesignSystem from "./src/design-system"
 import * as recipes from "./src/utilities/color";
 import fs from "fs";
 
-const keep = [
+const recipeNames = [
     "neutralForegroundRest",
     "neutralForegroundHover",
     "neutralForegroundActive",
@@ -49,30 +49,53 @@ const keep = [
     "neutralLayerL2",
     "neutralLayerL3",
     "neutralLayerL4",
-].map((key) => {
-    return recipes[key](DesignSystem).toStringHexRGB().toUpperCase()
-});
+];
 
-fs.writeFileSync("./change-results.js", JSON.stringify(keep));
+// const results = recipeNames.map((key) => {
+//     return recipes[key](DesignSystem).toStringHexRGB().toUpperCase()
+// });
+// 
+// const baseline = JSON.parse(fs.readFileSync("./initial-results.js"));
+// 
+// const delta = baseline.reduce((accum, baselineColor, index) => {
+//     return baselineColor !== results[index]
+//         ? accum.concat(results[index])
+//         : accum
+// }, [])
+// 
+// 
+// console.log(delta)
 
-// var suite = new Benchmark.Suite();
+// fs.writeFileSync("./change-results.js", JSON.stringify(keep, null, 2));
+//
+var suite = new Benchmark.Suite();
+// const designSystems = DesignSystem.neutralPalette.map((color) => Object.assign({}, DesignSystem, { backgroundColor: color }));
+const designSystems = [Object.assign({}, DesignSystem, { backgroundColor: DesignSystem.neutralPalette[Math.floor(DesignSystem.neutralPalette.length / 2)] })]
+
+recipeNames.forEach((recipeName) => {
+    designSystems.forEach((designSystem) => {
+        suite.add(`${recipeName},${designSystem.backgroundColor.toStringHexRGB().toUpperCase()}`, () => recipes[recipeName](designSystem).toStringHexRGB())
+    });
+})
+
+
 // DesignSystem.neutralPalette
 //     .map((color) => Object.assign({}, DesignSystem, { backgroundColor: color }))
 //     .forEach((designSystem) => {
 //         suite.add("baseline " + designSystem.backgroundColor, () => neutralForegroundHint(designSystem))
 //     });
 // 
-// suite.on("complete", function() {
-//     const body = Object.keys(this)
-//         .filter(key => this[key] && this[key].name && this[key].hz)
-//         .map(key => ({
-//             functionName: this[key].name,
-//             ops: this[key].hz,
-//             opsMs: this[key].hz / 1000,
-// 
-//         }));
-//     fs.writeFileSync("./results.post-memoize.json", JSON.stringify(body, null, 4));
-// 
-// });
-// 
-// suite.run();
+suite.on("complete", function() {
+    const body = Object.keys(this)
+        .filter(key => this[key] && this[key].name && this[key].hz)
+        .map(key => ({
+            functionName: this[key].name,
+            ops: this[key].hz,
+            opsMs: this[key].hz / 1000,
+
+        }));
+    fs.writeFileSync("./recipes-post.json", JSON.stringify(body, null, 4));
+
+});
+
+suite.run();
