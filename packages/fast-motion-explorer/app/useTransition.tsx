dcc2151f5dd9c5ef44f8815f476df69a31263f39
Nick Rice <nicholas.rice119@gmail.com>
2019-11-07 09:27:49 -0800

    progress on useTransition

--- packages/fast-motion-explorer/app/useTransition.tsx
+++ packages/fast-motion-explorer/app/useTransition.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { EventListener } from "popmotion/lib/input/listen/types";
 import { isNumber } from "util";
+import { isFunction } from "@babel/types";
 
 export enum TransitionStates {
     from,
@@ -8,6 +9,57 @@ export enum TransitionStates {
     out,
 }
 
+export function useInterval(callback: () => any, delay: number | null): void {
+    const savedCallback: React.MutableRefObject<(() => any) | undefined> = useRef();
+
+    useEffect(
+        () => {
+            savedCallback.current = callback;
+        },
+        [callback]
+    );
+
+    useEffect(
+        () => {
+            function tick(): void {
+                if (isFunction(savedCallback.current)) {
+                    savedCallback.current();
+                }
+            }
+
+            if (delay !== null) {
+                const id: number = window.setInterval(tick, delay);
+                return (): void => window.clearInterval(id);
+            }
+        },
+        [delay]
+    );
+}
+
+// function useInterval(callback, delay) {
+//     const savedCallback = useRef();
+//
+//     //  Remember the latest callback.
+//     useEffect(() => {
+//         savedCallback.current = callback;
+//     }, [callback]);
+//
+//     // Set up the interval.
+//     useEffect(() => {
+//         function tick() {
+//             savedCallback.current();
+//         }
+//         if (delay !== null) {
+//             let id = setInterval(tick, delay);
+//             return () => clearInterval(id);
+//         }
+//     }, [delay]);
+// }
+//                                    }
+//                      }
+//                  })
+//        })
+// }
 const listener: Map<symbol, number | void> = new Map(); // This won't work for multiple items, need a way to have a listener context for each function
 export function useTransition(
     value: boolean,
@@ -29,15 +81,16 @@ export function useTransition(
                 ? TransitionStates.from
                 : TransitionStates.out;
 
-    if (value !== visible && !isNumber(id)) {
-        listener.set(
-            id,
-            window.setTimeout(() => {
-                listener.set(id, window.clearTimeout(listener.get(id) as number));
-                setVisible(value);
-            }, isNumber(duration) ? duration : state === TransitionStates.to ? duration.in : duration.out)
-        );
-    }
+    const dur: number = isNumber(duration)
+        ? duration
+        : state === TransitionStates.to
+            ? duration.in
+            : duration.out;
+    useInterval(() => {
+        if (value !== visible && !isNumber(id)) {
+            setVisible(value);
+        }
+    }, dur);
 
     return state;
 }
