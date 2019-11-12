365f3255d286c72eb8b5959f687ef5b774309762
Nick Rice <nicholas.rice119@gmail.com>
2019-11-12 12:26:33 -0800

    adding useTimeout hook

---
+++ packages/fast-components-react-msft/src/utilities/hooks/use-timeout.spec.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import Adapter from "enzyme-adapter-react-16";
+import { configure, mount, ReactWrapper, shallow } from "enzyme";
+import { useTimeout } from "./use-timeout";
+
+/*
+ * Configure Enzyme
+ */
+configure({ adapter: new Adapter() });
+
+/**
+ * Mock timers
+ */
+jest.useFakeTimers();
+
+function UseTimeout(props: { timeout: number | null; callback: () => any }): JSX.Element {
+    useTimeout(props.callback, props.timeout);
+
+    return <div />;
+}
+
+describe("use-timeout", (): void => {
+    test("should throw if the provided callback is not a function", (): void => {
+        const rendered: ReactWrapper = mount(
+            <UseTimeout timeout={300} callback={null} />
+        );
+
+        expect(() => {
+            jest.runAllTimers();
+        }).toThrow();
+    });
+
+    test("should call a callback after the provided period of time", (): void => {
+        const spy: jest.Mock = jest.fn();
+        const rendered: ReactWrapper = mount(<UseTimeout timeout={300} callback={spy} />);
+
+        jest.runAllTimers();
+        expect(spy).toHaveBeenCalledTimes(1);
+    });
+    test("should provided callback when delay is changed", (): void => {
+        const spy: jest.Mock = jest.fn();
+        const rendered: ReactWrapper = mount(<UseTimeout timeout={300} callback={spy} />);
+
+        rendered.setProps({ delay: 200 });
+
+        jest.runAllTimers();
+        expect(spy).toHaveBeenCalledTimes(1);
+    });
+    test("should call new callback when one is provided", (): void => {
+        const spy: jest.Mock = jest.fn();
+        const spy2: jest.Mock = jest.fn();
+        const rendered: ReactWrapper = mount(<UseTimeout timeout={300} callback={spy} />);
+
+        rendered.setProps({ callback: spy2 });
+
+        jest.runAllTimers();
+        expect(spy).toHaveBeenCalledTimes(0);
+        expect(spy2).toHaveBeenCalledTimes(1);
+    });
+    test("should not call timer if component unmounts before timeout", (): void => {
+        const spy: jest.Mock = jest.fn();
+        const rendered: ReactWrapper = mount(<UseTimeout timeout={300} callback={spy} />);
+
+        rendered.unmount();
+        jest.runAllTimers();
+
+        expect(spy).toHaveBeenCalledTimes(0);
+    });
+    test("should not invoke callback if timeout is set to null", (): void => {
+        const spy: jest.Mock = jest.fn();
+        const rendered: ReactWrapper = mount(<UseTimeout timeout={300} callback={spy} />);
+        rendered.setProps({ timeout: null });
+
+        jest.runAllTimers();
+
+        expect(spy).toHaveBeenCalledTimes(0);
+    });
+});
---
+++ packages/fast-components-react-msft/src/utilities/hooks/use-timeout.ts
@@ -0,0 +1,30 @@
+import { useEffect, useRef } from "react";
+import { isFunction } from "lodash-es";
+
+/**
+ * React hook to call set a timeout
+ */
+export function useTimeout(callback: () => any, delay: number | null): void {
+    const savedCallback: React.MutableRefObject<(() => any)> = useRef(callback);
+
+    useEffect(
+        () => {
+            savedCallback.current = callback;
+        },
+        [callback]
+    );
+
+    useEffect(
+        () => {
+            function tick(): void {
+                savedCallback.current();
+            }
+
+            if (delay !== null) {
+                const id: number = window.setTimeout(tick, delay);
+                return (): void => window.clearTimeout(id);
+            }
+        },
+        [delay]
+    );
+}
