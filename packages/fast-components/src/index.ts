faf7bf13efca2d50e7b6457a3966a2e7bf3031dd
Stephane Comeau <scomea@microsoft.com>
2020-02-28 19:13:14 -0800

    working

--- packages/fast-components/src/index.ts
+++ packages/fast-components/src/index.ts
@@ -2,6 +2,7 @@ export * from "./checkbox";
 export * from "./design-system-provider";
 export * from "./dialog";
 export * from "./name-tag";
+export * from "./anchored-region";
 export * from "./switch";
 export * from "./text-area";
 export * from "./text-field";
