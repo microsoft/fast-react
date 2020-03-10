647ce1685e75080d740fdb9d5f2d37f858c5668b
nicholasrice <nicholas.rice119@gmail.com>
2020-03-10 12:59:51 -0700

    error

--- packages/fast-components/src/slider/slider.template.ts
+++ packages/fast-components/src/slider/slider.template.ts
@@ -1,32 +1,10 @@
 import { html, when } from "@microsoft/fast-element";
 import { Slider } from "./slider";
-//import { bool } from "../utilities";
 import { ref } from "@microsoft/fast-element/src/directives";
 
 export const SliderTemplate = html<Slider>`
     <div
-        part="slider"
-        class="slider"
+        ${ref("backgroundTrack")}
     >
-    <slot name="label">
-      <div>${x => x.label}</div>
-    </slot>
-    <div part="layout-region" class="layout-region">
-      <div ${ref("backgroundTrack")} part="background-track" class="background-track">
-        <slot name="background-track"></slot>
-      </div>
-      <slot name="track">
-        <div part="track" class="track"></div>
-      </slot>
-      <slot name="thumb">
-        <div part="thumb" class="thumb" style=${x => x.position}></div>
-      </slot>
-      ${when(
-          x => x.childNodes.length,
-          html`
-          <slot></slot>
-      `
-      )}
-      </div>
     </div>
 `;
