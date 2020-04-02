25c64c7ff2800c3879fcde9fd845a2909950b44c
nicholasrice <nicholas.rice119@gmail.com>
2020-04-02 15:33:41 -0700

    add logs for lifecycle event

--- packages/fast-components/src/name-tag/name-tag.ts
+++ packages/fast-components/src/name-tag/name-tag.ts
@@ -3,4 +3,14 @@ import { attr, FastElement } from "@microsoft/fast-element";
 export class NameTag extends FastElement {
     @attr
     public greeting: string = "Hello";
+
+    constructor() {
+        super();
+
+        console.log("name-tag constructor fired");
+    }
+
+    public connectedCallback() {
+        console.log("name-tag connectedCallback fired");
+    }
 }
