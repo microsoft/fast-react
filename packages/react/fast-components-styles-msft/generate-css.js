2632fce24eeee6d76de03078664c05b2afe5fde3
nicholasrice <nicholas.rice119@gmail.com>
2020-04-16 16:21:55 -0700

    add css generation

--- packages/react/fast-components-styles-msft/generate-css.js
+++ packages/react/fast-components-styles-msft/generate-css.js
@@ -1,22 +1,115 @@
+/* eslint-disable */
 const jss = require("jss").default;
 const preset = require("jss-preset-default").default;
-
+const path = require("path");
 // import * as preset from "jss-preset-default";
-import { AccentButtonStyles, ButtonStyles, CardStyles } from "./src";
 import fs from "fs";
+import {
+    AccentButtonStyles,
+    ActionToggleStyles,
+    ActionTriggerStyles,
+    AutoSuggestStyles,
+    AutoSuggestOptionStyles,
+    BadgeStyles,
+    BreadcrumbStyles,
+    ButtonStyles,
+    CallToActionStyles,
+    CaptionStyles,
+    CardStyles,
+    CarouselStyles,
+    ContextMenuStyles,
+    ContextMenuItemStyles,
+    DialogStyles,
+    DividerStyles,
+    FlipperStyles,
+    FlyoutStyles,
+    HeadingStyles,
+    HypertextStyles,
+    ImageStyles,
+    LabelStyles,
+    LightweightButtonStyles,
+    MetatextStyles,
+    NeutralButtonStyles,
+    NumberFieldStyles,
+    OutlineButtonStyles,
+    ParagraphStyles,
+    PivotStyles,
+    ProgressStyles,
+    RadioStyles,
+    SelectStyles,
+    SelectOptionStyles,
+    SliderStyles,
+    SliderLabelStyles,
+    StealthButtonStyles,
+    SubheadingStyles,
+    TextActionStyles,
+    TextAreaStyles,
+    TextFieldStyles,
+    ToggleStyles,
+    TreeViewStyles,
+    TreeViewItemStyles,
+    TypographyStyles,
+} from "./src";
 
 function generateClassName(rule, sheet) {
     return rule.key;
 }
 
 jss.setup(preset());
-const style = jss
-    .createStyleSheet(ButtonStyles, { generateClassName })
-    .update({})
-    .toString();
+jss.setup({ generateClassName });
 
-fs.mkdir("./css/button/", { recursive: true }, err => {
-    if (err) throw err;
+[
+    ["accent-button", AccentButtonStyles],
+    ["action-toggle", ActionToggleStyles],
+    ["action-trigger", ActionTriggerStyles],
+    ["auto-suggest", AutoSuggestStyles],
+    ["auto-suggest-option", AutoSuggestOptionStyles],
+    ["badge", BadgeStyles],
+    ["breadcrumb", BreadcrumbStyles],
+    ["button", ButtonStyles],
+    ["call-to-action", CallToActionStyles],
+    ["caption", CaptionStyles],
+    ["card", CardStyles],
+    ["carousel", CarouselStyles],
+    ["context-menu", ContextMenuStyles],
+    ["Context-menu-item", ContextMenuItemStyles],
+    ["dialog", DialogStyles],
+    ["divider", DividerStyles],
+    ["flipper", FlipperStyles],
+    ["flyout", FlyoutStyles],
+    ["heading", HeadingStyles],
+    ["hypertext", HypertextStyles],
+    ["image", ImageStyles],
+    ["label", LabelStyles],
+    ["lightweight-button", LightweightButtonStyles],
+    ["metatext", MetatextStyles],
+    ["neutralButton", NeutralButtonStyles],
+    ["number-field", NumberFieldStyles],
+    ["outline-button", OutlineButtonStyles],
+    ["paragraph", ParagraphStyles],
+    ["pivot", PivotStyles],
+    ["progress", ProgressStyles],
+    ["radio", RadioStyles],
+    ["select", SelectStyles],
+    ["select-option", SelectOptionStyles],
+    ["slider", SliderStyles],
+    ["slider-label", SliderLabelStyles],
+    ["stealth-button", StealthButtonStyles],
+    ["subheading", SubheadingStyles],
+    ["text-action", TextActionStyles],
+    ["text-area", TextAreaStyles],
+    ["text-field", TextFieldStyles],
+    ["toggle", ToggleStyles],
+    ["tree-view", TreeViewStyles],
+    ["tree-view-item", TreeViewItemStyles],
+    ["typography", TypographyStyles],
+].forEach(pair => {
+    const style = jss.createStyleSheet(pair[1]);
+    const outDir = path.resolve(process.cwd(), "css");
 
-    fs.writeFileSync("./css/button/style.css", style);
+    fs.mkdir(outDir, { recursive: true }, err => {
+        if (err) throw err;
+
+        fs.writeFileSync(path.resolve(outDir, `${pair[0]}.css`), style);
+    });
 });
