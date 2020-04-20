b906be9ef72bb5f118aadefd977901f82afb4836
nicholasrice <nicholas.rice119@gmail.com>
2020-04-20 16:51:03 -0700

    fix build script

--- build-css-module-packages.js
+++ build-css-module-packages.js
@@ -3,14 +3,24 @@ const path = require("path");
 const names = [
     "fast-components-react-msft",
     "fast-components-styles-msft",
-    // "fast-jss-manager-react",
-    // "fast-jss-utilities"
+    "fast-jss-manager-react",
+    "fast-jss-utilities",
 ];
+const fs = require("fs");
 const { execSync } = require("child_process");
+
+const outDir = path.resolve(__dirname, "tarballs");
+
+if (!fs.existsSync(outDir)) {
+    fs.mkdirSync(outDir, { recursive: true });
+}
 
 for (name of names) {
     const dirpath = path.resolve(__dirname, "packages", "react", name);
 
-    console.log(execSync("npm pack", { cwd: dirpath }).toString());
+    const stdout = execSync("npm pack", { cwd: dirpath }).toString();
+    const tarball = stdout.match(/(microsoft.+tgz)/g)[0];
+
+    fs.renameSync(path.resolve(dirpath, tarball), path.resolve(outDir, tarball));
 }
-/* eslint-endable */
+/* eslint-enable */
